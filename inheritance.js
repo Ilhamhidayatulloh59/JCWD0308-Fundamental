2320763ad015c34ef409627b36b341d88f23bd60
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-02-16 13:30:44 +0700

    instanceof

--- inheritance.js
+++ inheritance.js
@@ -30,8 +30,8 @@ const buku1 = new TereLiye("Bumi", 100)
 const buku2 = new TereLiye("Bulan", 120)
 const buku3 = new Hernan("Kalkulus", 300)
 
-console.log(buku1)
+console.log(buku1 instanceof TereLiye) // true
 console.log(buku2)
-console.log(buku3)
+console.log(buku3 instanceof TereLiye) // false
 
 
