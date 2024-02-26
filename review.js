87091e7d1dd9ad8ef7518faef03b80c32870ab4f
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-02-26 15:58:31 +0700

    Review

---
+++ review.js
@@ -0,0 +1,153 @@
+// console.log(user)
+
+// var user = "john";
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.sort((a, b) => b - a)
+// arr = arr.map((item) => item + 2)
+
+// let asc = arr.sort((a, b) => a - b)
+// let desc = [...arr].sort((a, b) => b - a)
+// let even = arr.filter((item) => item % 2 === 0)
+// let desc = arr.reverse()
+// let newArr = arr.push(6)
+// let str = arr.join(" ")
+// let newArr = arr.splice(2, 1)
+// let newArr = arr.shift()
+
+// console.log(arr);
+// console.log(desc);
+
+// var num = 10
+
+// {
+//     var num = 20
+//     console.log(num);
+// }
+// var num = 10
+
+// console.log(num)
+// let num = 0
+// for (let i = 0; i < 5; i++) {
+//     num = i
+// }
+// console.log(num);
+
+// let num = 0
+// let i = 5
+// do {
+//     ++i
+//     num = i
+// } while (i < 0);
+
+// console.log(num);
+
+// let i = 10
+// ++i
+// console.log(i)
+
+// let name = "1"
+
+// if (+name && true) {
+//     console.log(true)
+// } else {
+//     console.log(false);
+// }
+
+// let num = "10"
+
+// console.log(typeof num)
+// // Number(num) - +num
+// console.log(typeof +num)
+
+// let name = "abid"
+
+// function setBudi() {
+//     myFunc("budi")
+// }
+
+// function setDhika() {
+//     myFunc("dhika")
+// }
+
+// console.log(name, setBudi(), setDhika(), name)
+
+// function myFunc(str) {
+//     name = str
+// }
+
+// function getData(age = 17) {
+//     return {
+//         name: 'dhika',
+//         city: 'bandung',
+//         age: age
+//     }
+// }
+
+// const { city, name, age } = getData(20)
+// console.log(age);
+
+// let obj = { a: 1, b: 3 };
+// let obj2 = { b: 2, c: 2 };
+
+// console.log({ ...obj, ...obj2 });
+
+// let arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+
+// console.log(arr.flat().reduce((a, b) => a + b))
+
+// let arr = [0, 12, 23, 102, 8, 7]
+
+// let newArr = arr.reverse()
+
+// console.log(arr);
+
+// let word = "Hello word"
+// word.split(" ").reverse()
+// console.log(word)
+
+
+// let str = "apple"
+
+// console.log(10 - 10 + str)
+
+// let arr = []
+
+// console.log(2 / 0.5)
+
+// let person = {}
+// person['name'] = "John"
+// person['age'] = 20
+// person['address'] = "Jakarta"
+
+// let obj = { ...person }
+// obj.address = "Bandung"
+
+// console.log(JSON.stringify(person));
+// console.log(JSON.stringify(obj));
+
+// person.address = "Jakarta"
+
+// var person = {
+//     name: "John",
+//     age: 20
+// }
+
+// name = "Budi"
+// name = "Smith"
+
+// console.log(name)
+
+
+
+
+
+
+
+
+
+
